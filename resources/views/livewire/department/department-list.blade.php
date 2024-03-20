bd3785b3ec2214274725731d9f7e0edad5bc6ebe
Ethan Chang <zyyhaw@gmail.com>
2024-03-20 12:17:58 +0800

    update tenant

--- resources/views/livewire/department/department-list.blade.php
+++ resources/views/livewire/department/department-list.blade.php
@@ -2,7 +2,7 @@
 
 
     <!-- Create Department Modal -->
-    <div wire:ignore.self class="modal fade" id="createDepartmentModal" data-backdrop="static" tabindex="-1" aria-hidden="true">
+    <div wire:ignore.self class="modal fade" id="createDepartmentModal1" data-backdrop="static" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header bg-transparent">
@@ -34,6 +34,112 @@
         </div>
     </div>
     <!--/ Add Permission Modal -->
+    <div class="modal modal-slide-in fade" id="createDepartmentModal">
+        <div class="modal-dialog sidebar-sm">
+            <div class="add-new-record modal-content pt-0" id="modal_env_form" name="modalenvform">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">×</button>
+                <div class="modal-header mb-1">
+                    <h5 class="modal-title" id="form-label" name="form-label">New Record</h5>
+                </div>
+
+                <div class="modal-body flex-grow-1">
+                    <form class="needs-validation" novalidate id="envform" name="envform"  method="POST" accept-charset="UTF-8">
+                        <input class="hidden"  name="_token" value="{{ csrf_token() }}">
+                        <input class="hidden" name="form_id" id="form_id" value="">
+                        <div class="mb-1">
+                            <label class="form-label" for="basic-addon-name">Name</label>
+
+                            <input
+                                type="text"
+                                id="basic-addon-name"
+                                name="basic_addon_name"
+                                class="form-control"
+                                placeholder="e.g. Windows"
+                                aria-label="Name"
+                                required
+                                aria-describedby="basic-addon-name"
+                            />
+                            <div class="valid-feedback">Looks good!</div>
+                            <div class="invalid-feedback">Please enter operating system name.</div>
+                        </div>
+                        <div class="mb-1">
+                            <label class="form-label" for="basic-default-display-name">Display Name</label>
+                            <input
+                                type="text"
+                                id="basic-default-display-name"
+                                name="basic_default_display_name"
+
+                                class="form-control"
+                                placeholder="MS Window 11"
+                                aria-label="Display Name"
+                                required
+                            />
+                            <div class="valid-feedback">Looks good!</div>
+                            <div class="invalid-feedback">Please enter a display name</div>
+                        </div>
+                        <div class="mb-1">
+                            <label class="form-label" for="basic-default-password1">Description</label>
+                            <input
+                                type="text"
+                                id="basic-default-desc"
+                                name="basic_default_desc"
+                                class="form-control"
+                                placeholder="Short sentence about operating system.."
+                                minlength="5"
+                                required
+                            />
+                            <div class="valid-feedback">Looks good!</div>
+                            <div class="invalid-feedback">Please enter your sentence.</div>
+                        </div>
+
+                        <div class="mb-1">
+                            <label class="form-label" for="basic-default-password1">Cost</label>
+                            <input
+                                type="decimal"
+                                id="basic-default-cost"
+                                name="basic_default_cost"
+                                class="form-control"
+                                placeholder="$10"
+                                min="1"
+                                required
+                                pattern="^\d*(\.\d{0,2})?$"
+                            />
+                            <small>This cost is Cost per day(temporary decision)</small>
+                            <div class="valid-feedback">Looks good!</div>
+                            <div class="invalid-feedback">Please enter your Cost.</div>
+                        </div>
+
+                        <div class="mb-1">
+                            <label class="form-label" for="select-os-platform">OS Platform</label>
+                            <select class="form-select" id="select-os-platform" name="select_os_platform" required>
+                                <option value="">Select OS Platform</option>
+                                <option value="windows">Windows</option>
+                                <option value="rhel">RHEL</option>
+                                <option value="centos">Centos</option>
+                                <option value="other">Other</option>
+                            </select>
+                            <div class="valid-feedback">Looks good!</div>
+                            <div class="invalid-feedback">Please select Operating System Platform</div>
+                        </div>
+                        <div class="mb-1">
+                            <label class="form-label" for="select-status">Publish</label>
+                            <select class="form-select" id="select-status" name="select_status" required>
+                                <option value="1">Yes</option>
+                                <option value="0">No</option>
+                            </select>
+                            <div class="valid-feedback">Looks good!</div>
+                            <div class="invalid-feedback">Please select colour label</div>
+                        </div>
+
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 
 
     <section>
@@ -43,7 +149,7 @@
             <h5 class="card-header"></h5>
             <div class="flex items-center justify-between d p-4">
                 <div class="flex">
-                    <button class="btn btn-sm  mt-50 btn-primary" style="float: right;" data-toggle="modal" data-target="#createDepartmentModal">Create New Department</button>
+                    <button class="btn btn-primary" style="float: right;" data-toggle="modal" data-target="#createDepartmentModal">Create New Department</button>
                 </div>
                 <div class="flex">
                     <div class="relative w-full">
